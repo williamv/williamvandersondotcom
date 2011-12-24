f80b6a7917ea7c704f9ac7307947bd49b4102d6d
williamv <williamv.anderson@gmail.com>
2011-12-24 12:07:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Williamvandersondotcom::Application.config.secret_token = 'e5c3ab2b18041f13b0e50296bcb4a354dece87599b49f8c1ae780038368cf8eadc81f6f7045cb061257dd9adb3203d3603370689bf9f9a84e3eec82df1b93042'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Williamvandersondotcom::Application.config.session_store :cookie_store, key: '_williamvandersondotcom_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Williamvandersondotcom::Application.config.session_store :active_record_store
